f6f45ad08ae332f38441385a91a19c9f01d4d8c2
NeilVakharia45 <neildvakharia@gmail.com>
2026-03-08 16:39:56 -0400

    Changes to bash script - reduced num envs to 1

--- train.bash
+++ train.bash
@@ -10,9 +10,15 @@
 
 module load tacc-apptainer
 
-singularity exec --nv $SCRATCH/diff-cam.sif \
+singularity exec --nv \
+    --bind $SCRATCH/diff-cam/runs:/app/runs \
+    --bind $SCRATCH/diff-cam/simulator:/opt/conda/lib/python3.11/site-packages/simulator \
+    --bind $SCRATCH/diff-cam/cam_env:/opt/conda/lib/python3.11/site-packages/cam_env \
+    --bind $SCRATCH/diff-cam/algorithms:/app/algorithms \
+    $WORK/diff-cam.sif \
     python3 /app/algorithms/ppo.py \
     --total_timesteps 10000000 \
-    --num_envs 16 \
+    --num_envs 1 \
+    --resolution 1024 \
     --num_steps 2048 \
     --max_steps 1024
